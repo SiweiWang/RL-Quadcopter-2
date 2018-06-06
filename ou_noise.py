2423433bdfd92705d1a9d8a8c9e53869ec426c2a
Siwei <wangsiwei2003@hotmail.com>
2018-06-06 08:45:23 -0400

    add noise class

---
+++ ou_noise.py
@@ -0,0 +1,22 @@
+import numpy as numpy
+import copy
+
+class OUNoise:
+    """Ornstein-Uhlenbeck process."""
+
+    def __init__(self, size, mu, theta, sigma):
+        self.mu = mu* np.ones(size)
+        self.theta = theta
+        self.sigma = sigma
+        self.reset()
+
+    def reset(self):
+        """Reset the internal state (= noise) to mean (mu). """
+
+        slef.state = copy.copy(self.mu)
+
+    def sample(self):
+        x = self.state
+        dx = self.theta * (self.mu -x) + self.sigma * np.random.randn(len(x))
+        self.state = x + dx
+        return self.state
